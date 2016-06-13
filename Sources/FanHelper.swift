790b15ceb94da1c739ced8db6e6906f64e62915d
赵少龙 <orangeince@outlook.com>
2016-06-13 23:16:08 +0800

    a

--- Sources/FanHelper.swift
+++ Sources/FanHelper.swift
@@ -1,7 +1,7 @@
-import cURL
+import PerfectCURL
+//import cURL
 
 class FlanHelper {
-    static var cUrl = CURL("https://hook.bearychat.com/=bw85y/incoming/1d6cc882252511592d627cfab2bfe99d")
     static func fanOrderFor(_ user: String) {
         let postString = "\"text\": \"fan +1 \(user)\""
         performRequestWith(postString)
@@ -11,6 +11,7 @@ class FlanHelper {
         performRequestWith(postString)
     }
     static func performRequestWith(_ postString: String) {
+        let cUrl = CURL(url: "https://hook.bearychat.com/=bw85y/incoming/1d6cc882252511592d627cfab2bfe99d")
         cUrl.setOption(CURLOPT_HTTPHEADER, s: "Content-Type: application/json")
         cUrl.setOption(CURLOPT_POSTFIELDS, s: postString)
         cUrl.perform{
