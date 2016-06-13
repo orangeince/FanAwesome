c5cd8056cc6c4b58281a6aef741542b2400ef1e1
赵少龙 <orangeince@outlook.com>
2016-06-13 23:53:02 +0800

    a

--- Sources/FanHelper.swift
+++ Sources/FanHelper.swift
@@ -14,7 +14,7 @@ class FlanHelper {
         //let cUrl = CURL(url: "https://hook.bearychat.com/=bw85y/incoming/1d6cc882252511592d627cfab2bfe99d")
         let cUrl = CURL(url: "http://182.92.65.174:8181")
         _ = cUrl.setOption(CURLOPT_HTTPHEADER, s: "Content-Type: application/json")
-        let v = curl_easy_escape(cUrl, postString, 0)
+        let v = curl_easy_escape(cUrl.curl!, postString, 0)
         _ = cUrl.setOption(CURLOPT_POSTFIELDS, v: v)
         cUrl.perform{
             code, header, body in
