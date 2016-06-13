06fddb34cfcbcdfea1c00a1a6e8678f305ea464a
赵少龙 <orangeince@outlook.com>
2016-06-13 23:20:08 +0800

    d

--- Sources/FanHelper.swift
+++ Sources/FanHelper.swift
@@ -12,14 +12,14 @@ class FlanHelper {
     }
     static func performRequestWith(_ postString: String) {
         let cUrl = CURL(url: "https://hook.bearychat.com/=bw85y/incoming/1d6cc882252511592d627cfab2bfe99d")
-        cUrl.setOption(CURLOPT_HTTPHEADER, s: "Content-Type: application/json")
-        cUrl.setOption(CURLOPT_POSTFIELDS, s: postString)
+        _ = cUrl.setOption(CURLOPT_HTTPHEADER, s: "Content-Type: application/json")
+        _ = cUrl.setOption(CURLOPT_POSTFIELDS, s: postString)
         cUrl.perform{
             code, header, body in
             if code == 0 {
                 print("performRequest Success!! post: \(postString)")
             } else {
-                let respose = curl.resposeCode
+                let respose = cUrl.resposeCode
                 print("performRequest failed... code: \(respose) post: \(postString)")
             }
         }
