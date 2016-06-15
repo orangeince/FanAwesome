fbc4b29f15fc92fd14b3362580816c1c1a62f230
赵少龙 <orangeince@outlook.com>
2016-06-15 23:23:44 +0800

    a

--- Sources/FanHelper.swift
+++ Sources/FanHelper.swift
@@ -11,8 +11,8 @@ class FlanHelper {
         performRequestWith(postString)
     }
     static func performRequestWith(_ postString: String) {
-        let cUrl = CURL(url: "https://hook.bearychat.com/=bw85y/incoming/1d6cc882252511592d627cfab2bfe99d")
-        //let cUrl = CURL(url: "http://182.92.65.174:8181")
+        //let cUrl = CURL(url: "https://hook.bearychat.com/=bw85y/incoming/1d6cc882252511592d627cfab2bfe99d")
+        let cUrl = CURL(url: "http://182.92.65.174:8181")
         let byteArray = [UInt8](postString.utf8)
 
         _ = cUrl.setOption(CURLOPT_HTTPHEADER, s: "HTTP/1.1")
