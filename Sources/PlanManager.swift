89c1dd6ac409b35959c63ca4f6df30432dfbd1fc
赵少龙 <orangeince@outlook.com>
2016-06-29 22:45:26 +0800

    a

--- Sources/PlanManager.swift
+++ Sources/PlanManager.swift
@@ -298,7 +298,7 @@ struct PlanManager {
 func getFormattedDateOfToday() -> Int {
     let today = NSDate()
     let calendar = NSCalendar.currentCalendar()
-    let dateComponents = calendar.components([.Day, .Month], fromDate: today)
+    let dateComponents = calendar.components([.Day, .Month], from: today)
     return dateComponents.month * 100 + dateComponents.day
 }
 
